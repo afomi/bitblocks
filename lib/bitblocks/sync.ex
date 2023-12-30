733d07253cb26316ba03550c54f97c1917da7289
Ryan Wold <>
2023-12-30 01:41:54 -0800

    clunky sync

--- lib/bitblocks/sync.ex
+++ lib/bitblocks/sync.ex
@@ -1,9 +1,122 @@
 defmodule Bitblocks.Sync do
+  require IEx
+
+  import Ecto.Query, warn: false
+  alias Bitblocks.Repo
+
+  def get_all(range) do
+    Enum.each(range, fn block ->
+      get(block)
+    end)
+  end
+
+  def get(block_height) do
+
+    # blocks = Bitblocks.Repo.all(query)
+    query = from i in Bitblocks.Block,
+        where: (i.height == ^block_height), limit: 1
+    block = Bitblocks.Repo.one(query)
+
+    case BitcoinsvCli.getblock(block.hash, level = 2) do
+      {:error, %HTTPoison.Error{reason: :connect_timeout, id: nil}} ->
+        IO.puts "Errrrrrr Connect_timeout"
+
+      {:error, %HTTPoison.Error{reason: :closed, id: nil}} ->
+        IO.puts "Errrrrrr Closed"
+
+      {:error, %HTTPoison.Error{reason: :timeout, id: nil}} ->
+        IO.puts "Errrrrrr Timeout"
+
+      {:error, %HTTPoison.Error{reason: :enetunreach, id: nil}} ->
+        IO.puts "Errrrrrr enetunreach"
+
+      block_response ->
+        Enum.each(block_response["tx"], fn tx ->
+
+          txid = tx["txid"]
+          raw = tx["hex"]
+
+            t = %Bitblocks.Transaction{
+              txid: txid,
+              raw: raw,
+              block_hash: block.hash,
+              inputs: ["tx.inputs"],
+              outputs: ["tx.outputs"]
+            }
+
+            # what else to do to a transaction as it comes in.
+            # file explorer options and parser options / responsibilities
+
+            insert = t
+                |> Ecto.Changeset.change(%{})
+                |> Bitblocks.Repo.insert()
+
+            Process.sleep(250)
+
+            IO.puts "last wrote in block #{block_height}"
+
+            case insert do
+              {:ok, block} ->
+                "BLOCK INSERT" |> IO.puts
+                # block |> IO.inspect
+            end
+
+
+        end)
+    end
+
+
+  end
+
+  def get_original(block_height) do # 10:39am Friday Dec 29
+    query = from i in Bitblocks.Block,
+        where: (i.height == ^block_height), limit: 1
+    block = Bitblocks.Repo.one(query)
+
+
+    Enum.each(block.tx, fn txid ->
+
+      case BitcoinsvCli.getrawtransaction(txid) do
+
+        {:error, _} ->
+          IO.puts("SKIPPING #{txid}")
+
+        raw ->
+          BitcoinsvCli.decoderawtransaction(raw)
+
+          t = %Bitblocks.Transaction{
+            txid: txid,
+            raw: raw,
+            block_hash: block.hash,
+            inputs: ["tx.inputs"],
+            outputs: ["tx.outputs"]
+          }
+
+          # what else to do to a transaction as it comes in.
+          # file explorer options and parser options / responsibilities
+
+          insert = t
+              |> Ecto.Changeset.change(%{})
+              |> Bitblocks.Repo.insert()
+
+          Process.sleep(250)
+
+          IO.puts "last wrote in block #{block_height}"
+
+          case insert do
+            {:ok, block} ->
+              "BLOCK INSERT" |> IO.puts
+              # block |> IO.inspect
+          end
+      end
+
+    end)
+
+  end
 
   def loady(start, last) do
     skipped_blocks = []
 
-    # Enum.each(start..100_000, fn number ->
     Enum.each(start..last, fn number ->
       IO.puts(number) ###
 
@@ -58,6 +171,10 @@ defmodule Bitblocks.Sync do
         tx: tx,
       }
 
+      # Skip blocks that have more than 220,000 transactions (which is a lot after 2022),
+      # because that many 64-character txid's exceeds Mongodb's 16MB limit per field
+      #
+      # TODO: explore another datastore, like RocksDB or PostgreSQL
       if num_tx < 220_000 do
 
         insert = b
